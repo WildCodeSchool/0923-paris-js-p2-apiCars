1398e9e1db70f49382aafd23d78ff54b6af1f9cf
aurelien.mlk92 <aurelien.geninsimonet@gmail.com>
2023-10-31 17:22:45 +0100

    firstcommitaurel

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <ul>
+        <li>nous contactez</li>
+        <li>@aol.com</li>
+        <li>suivez nous</li>
+        <li>recrutement</li>
+        <li>pole-emploi.com</li>
+      </ul>
+    </div>
+  );
+}
+export default Footer;
