19228e476fa8722bd40f1668d8c7bce7e1349f67
27rachiid82 <mohammedikacirachid@yahoo.fr>
2023-11-07 16:48:24 +0100

    add components in page inscription

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Home from "./pages/Home/Home";
+import ListCategory from "./pages/ListCategory/ListCategory";
+import ListModelSearch from "./pages/ListModelSearch/ListModelSearch";
+import Reservation from "./pages/reservation/Reservation";
+import CarPage from "./pages/CarPage/CarPage";
+import Inscription from "./pages/Inscription/Inscription";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/Inscription",
+    element: <Inscription />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+  },
+  {
+    path: "/ListCategory",
+    element: <ListCategory />,
+  },
+  {
+    path: "/ListModelSearch",
+    element: <ListModelSearch />,
+  },
+  {
+    path: "/reservation",
+    element: <Reservation />,
+  },
+  {
+    path: "/CarPage",
+    element: <CarPage />,
   },
 ]);
 
@@ -18,30 +47,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-/*
-  {
-    path: "./pages/home"
-    element: <Home/>
-  },
-  {
-    path: "./pages/categorie"
-    element: <Categorie/>
-  },
-  {
-    path: "./pages/inscription"
-    element: <Inscription/>
-  }, 
-  {
-    path: "./pages/modele"
-    element: <Modele/>
-  },
-  {
-    path: "./pages/reservation"
-    element: <Reservation/>
-  }, 
-  {
-    path: "./pages/modele random"
-    element: <ModeleRandom/>
-  },
-  */
---
+++ frontend/src/pages/Inscription/Inscription.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import NavBar from "../../components/NavBar";
+import InscriptionForm from "../../components/InscriptionForm";
+import Banner from "../../components/Banner";
+
+function Inscription() {
+  return (
+    <>
+      <NavBar />
+      <Banner />
+      <InscriptionForm />
+      <Footer />
+    </>
+  );
+}
+
+export default Inscription;
