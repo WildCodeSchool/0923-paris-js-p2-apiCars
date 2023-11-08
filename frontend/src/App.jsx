a3d4dcb80dc4acc981bb297fa010f372f13020fc
martial jouan <martialjouan@icloud.com>
2023-11-08 10:24:34 +0100

    debut footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import ComponentCard from "./components/ComponentCard";
 import Inscription from "./components/Inscription";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <ComponentCard />
       <Inscription />
+      <Footer/>
     </>
   );
 }
