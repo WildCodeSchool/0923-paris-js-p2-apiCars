381a62503e7bfb03663ee0a007b6af5733c21215
brian boudrioux <brian.boudrioux@wildcodeschool.com>
2023-11-09 16:44:29 +0100

    merge banner

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -8,7 +8,7 @@ function Banner(props) {
         <p>
           {description}Chez API-CAR la mobilité nous tient a coeur, aujourd’hui
           vous pouvez louer des voitures de toute categorie au meilleur prix
-          c’est ça la magie API-CAR !!! API-CAR! BE API !!!
+          c’est ça la magie API-CAR !!! API-CAR, BE API !!!
         </p>
       </div>
     </div>
