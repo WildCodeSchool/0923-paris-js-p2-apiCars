a9ac347a6b002da89bae73a8778d1d0ec5db4f0f
27rachiid82 <mohammedikacirachid@yahoo.fr>
2023-11-10 16:59:40 +0100

    merge before repartition features

--- frontend/src/pages/Reservation/Reservation.jsx
+++ frontend/src/pages/Reservation/Reservation.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Footer from "../../components/Footer";
 import NavBar from "../../components/NavBar";
 import Banner from "../../components/Banner";
-import ComponentCard from "../../components/ComponentCard";
+import InscriptionForm from "../../components/InscriptionForm";
 
 function Reservation() {
   return (
     <>
       <NavBar />
       <Banner />
-      <ComponentCard />
+      <InscriptionForm />
       <Footer />
     </>
   );
