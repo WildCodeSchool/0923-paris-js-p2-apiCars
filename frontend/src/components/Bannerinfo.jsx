513d9135a5ab9bd57723e7dcf8ba7073de3b4f47
martial jouan <martialjouan@icloud.com>
2023-11-16 17:03:31 +0100

    3eme bannieres termine

---
+++ frontend/src/components/Bannerinfo.jsx
@@ -0,0 +1,13 @@
+import "./bannerInfo.css";
+
+function BannerInfo() {
+  return (
+    <div className="bannerInfo">
+      <div className="text">
+        <p>caractéristiques</p>
+      </div>
+    </div>
+  );
+}
+
+export default BannerInfo;
